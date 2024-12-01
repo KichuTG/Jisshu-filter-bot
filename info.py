a459e8bbb94713b1eb1c10d96c263a0e8ba36877
Kichu <kichumon333@outlook.com>
2024-12-01 15:25:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -97,7 +97,7 @@ LINK_MODE = is_enabled('LINK_MODE', True)
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or Flase
+STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or Flase
 # Online Stream and Download
 
 MULTI_CLIENT = False
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "0")
+URL = environ.get("FQDN", "https://required-scorpion-balala-ba3fe5d5.koyeb.app")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
